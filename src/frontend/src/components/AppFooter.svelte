56432e59427589ad807ca9fe0498bcc979971c3b
vold <vold.zavada@gmail.com>
2026-06-09 19:03:16 -0400

    fix footer and topbar url position

--- src/frontend/src/components/AppFooter.svelte
+++ src/frontend/src/components/AppFooter.svelte
@@ -3,8 +3,9 @@
     2026
     <a href="https://github.com/voldpix/mockboard" target="_blank" rel="noreferrer">mockboard</a>
   </span>
+  <span aria-hidden="true">/</span>
   <span>
-    made by
+    by
     <a href="https://github.com/voldpix" target="_blank" rel="noreferrer">voldpix</a>
   </span>
 </footer>
